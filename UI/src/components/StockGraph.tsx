4114c82e3f2bb3ab7d24e10f50fb64636d1cab8c
Craig McLellen <craig.mclellen@insight.com>
2025-02-15 08:44:40 +0800

    Fix

---
+++ UI/src/components/StockGraph.tsx
@@ -0,0 +1,5 @@
+function StockGraph() {
+  return <div>stock graph</div>;
+}
+
+export default StockGraph;
---
+++ UI/src/pages/AppLayout.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import StockGraph from "../components/StockGraph";
+
+function AppLayout() {
+  return (
+    <>
+      <Header />
+      <StockGraph />
+    </>
+  );
+}
+
+export default AppLayout;
