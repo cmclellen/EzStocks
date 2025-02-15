eed7877b0362fd4c7d5d9c9c9fd05bc315f0f64c
Craig McLellen <craig.mclellen@insight.com>
2025-02-15 10:57:16 +0800

    Fix

--- UI/src/components/Header.tsx
+++ UI/src/components/Header.tsx
@@ -9,6 +9,8 @@ import {
 } from "@headlessui/react";
 import { clsx } from "clsx";
 import { HiBars3, HiBell, HiXMark } from "react-icons/hi2";
+import stocksLogo from "../assets/stocks.png";
+import { AiOutlineStock } from "react-icons/ai";
 
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
@@ -17,34 +19,35 @@ const navigation = [
   { name: "Calendar", href: "#", current: false },
 ];
 
+function MobileMenu() {
+  return (
+    <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+      <span className="absolute -inset-0.5" />
+      <span className="sr-only">Open main menu</span>
+
+      <HiBars3
+        aria-hidden="true"
+        className="block size-6 group-data-open:hidden"
+      />
+      <HiXMark
+        aria-hidden="true"
+        className="hidden size-6 group-data-open:block"
+      />
+    </DisclosureButton>
+  );
+}
+
 function Header() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
+      <div className="mx-auto px-2 sm:px-6 lg:px-8 container">
+        <div className="relative flex h-16 items-center justify-between ">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-
-              <HiBars3
-                aria-hidden="true"
-                className="block size-6 group-data-open:hidden"
-              />
-              <HiXMark
-                aria-hidden="true"
-                className="hidden size-6 group-data-open:block"
-              />
-            </DisclosureButton>
+            <MobileMenu />
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 items-center">
-              <img
-                alt="Your Company"
-                src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
-              />
+            <div className="flex shrink-0 items-center text-white">
+              <AiOutlineStock size={30} />
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
--- UI/src/pages/AppLayout.tsx
+++ UI/src/pages/AppLayout.tsx
@@ -3,10 +3,12 @@ import StockGraph from "../components/StockGraph";
 
 function AppLayout() {
   return (
-    <>
+    <div className="h-dvh flex flex-col">
       <Header />
-      <StockGraph />
-    </>
+      <div className="grow border border-blue-600 container mx-auto justify-items-center content-center">
+        <StockGraph />
+      </div>
+    </div>
   );
 }
 
