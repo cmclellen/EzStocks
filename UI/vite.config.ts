6db8ea4a8a785a796f58e46ccd82ca7c00f03583
Craig McLellen <cmclellen@gmail.com>
2025-02-22 09:33:39 +0800

    Added proxy server (#2)

--- UI/vite.config.ts
+++ UI/vite.config.ts
@@ -4,4 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
+  server: {
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:7274/api",
+        secure: false,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
