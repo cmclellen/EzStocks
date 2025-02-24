8fac4ca06b7f97244a599f88706844b7ddb47808
Craig McLellen <craig.mclellen@icwa.wa.gov.au>
2025-02-25 05:44:12 +0800

    Fix

--- API/EzStocks.Api.Domain/Entities/StockItem.cs
+++ API/EzStocks.Api.Domain/Entities/StockItem.cs
@@ -3,6 +3,7 @@
     public class StockItem
     {
         public Guid Id { get; set; }
+        public required string Type { get; set; }
         public required string Code { get; set; }
         public required string Name { get; set; }
     }
---
+++ API/EzStocks.Api.Persistence/EntityTypeConfigurations/StockItemEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using EzStocks.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EzStocks.Api.Persistence.EntityTypeConfigurations
+{
+    public class StockItemEntityTypeConfiguration : IEntityTypeConfiguration<StockItem>
+    {
+        public void Configure(EntityTypeBuilder<StockItem> builder)
+        {
+            builder.ToContainer("StockItem");
+            builder.HasPartitionKey(s=>s.Type);
+        }
+    }
+}
