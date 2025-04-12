4501db82bb3effd39cb1abb0e8e980600e23c617
Craig McLellen <craig.mclellen@insight.com>
2025-04-12 22:46:51 +0000

    Fix

---
+++ API/EzStocks.Api.Application/Strategies/Normalize.cs
@@ -0,0 +1,11 @@
+﻿namespace EzStocks.Api.Application.Strategies
+{
+    public static class Normalize
+    {
+        public static decimal Transform(decimal price, decimal min, decimal max)
+        {
+            var decominator = max - min;
+            return decominator == 0 ? 1M : (price - min) / decominator;
+        }
+    }
+}
