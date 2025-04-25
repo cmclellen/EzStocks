48643788c8d424333cb6e76188a37e65b209c056
Craig McLellen <cmclellen@gmail.com>
2025-04-26 06:19:00 +0800

    Fix

--- UI/src/components/CheckBox.tsx
+++ UI/src/components/CheckBox.tsx
@@ -1,23 +1,54 @@
+import { useState } from "react";
+
 interface CheckBoxProps {
   title: string;
+  isChecked: boolean;
+  onChange: (checked: boolean) => void;
 }
 
-function CheckBox({ title }: CheckBoxProps) {
+function CheckBox(props: CheckBoxProps) {
+  const [isChecked, setIsChecked] = useState(props.isChecked || false);
+
+  console.log("isChecked", isChecked);
+
+  const handleCheckboxChanged = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const checked = event.target.checked;
+    setIsChecked(checked);
+    props.onChange(checked);
+  };
+
   return (
-    <div className="flex items-center mb-4">
+    <div className="flex items-center my-4 gap-2">
       <input
-        checked
+        checked={isChecked}
         id="checked-checkbox"
         type="checkbox"
-        value=""
-        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        onChange={handleCheckboxChanged}
+        className="relative peer shrink-0 appearance-none w-6 h-6 border-2 border-primary-500 rounded-sm bg-secondary checked:bg-secondary checked:border-2"
       />
       <label
-        for="checked-checkbox"
-        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        htmlFor="checked-checkbox"
+        className="text-md font-medium text-gray-900 dark:text-gray-300"
       >
-        {title}
+        {props.title}
       </label>
+      <svg
+        className="
+      absolute 
+      w-6 h-6 mt-1
+      hidden peer-checked:block pointer-events-none"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <polyline points="20 6 9 17 4 12"></polyline>
+      </svg>
     </div>
   );
 }
